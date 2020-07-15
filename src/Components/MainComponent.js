b58cd22040193f33c9b85d87c1e1b6cedd4b881c
Monzer Abdullaziz <wizmgbrown@gmail.com>
2020-07-15 10:37:54 +0200

    ASSM2

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -6,6 +6,7 @@ import {DISHES} from '../shared/dishes';
 import Dishdetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import About from './AboutComponent3';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
@@ -48,6 +49,7 @@ class Main extends Component {
               <Route path='/home' component={HomePage} />
               <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />
+              <Route path='/aboutus' component={() => <About leaders = {this.state.leaders}/>} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Redirect to="/home" />
           </Switch>
